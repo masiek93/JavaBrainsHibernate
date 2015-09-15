562a17d49175cd8402553ce460934907c4bb7ea4
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-15 12:32:52 +0200

    added column and table annotations

--- src/main/java/com/topyfi/javabrains/HibernateTutorial/dto/UserDetails.java
+++ src/main/java/com/topyfi/javabrains/HibernateTutorial/dto/UserDetails.java
@@ -1,12 +1,18 @@
 package com.topyfi.javabrains.HibernateTutorial.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table (name="USER_DETAILS")
 public class UserDetails {
 	@Id
+	@Column (name="USER_ID")
 	private int userId;
+	
+	@Column (name="USER_NAME")
 	private String userName;
 
 	public int getUserId() {
