e955613e2253462245cee082fee9449c76bfd159
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-16 13:04:06 +0200

    @OneToOne Mapping between UserDetails and Vehicle added

--- src/main/java/com/topyfi/javabrains/HibernateTutorial/Test.java
+++ src/main/java/com/topyfi/javabrains/HibernateTutorial/Test.java
@@ -1,63 +1,32 @@
 package com.topyfi.javabrains.HibernateTutorial;
 
-import java.util.Date;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.topyfi.javabrains.HibernateTutorial.dto.Address;
 import com.topyfi.javabrains.HibernateTutorial.dto.UserDetails;
+import com.topyfi.javabrains.HibernateTutorial.dto.Vehicle;
 
 public class Test {
 
 	public static void main(String[] args) {
 		UserDetails user = new UserDetails();
-		
-		Address address = new Address();
-		address.setStreet("Na Uboczu");
-		address.setCity("Warsaw");
-		address.setState("Mazowieckie");
-		address.setZipCode("33444");
-		
-		Address address2 = new Address();
-		address2.setStreet("Żytnia");
-		address2.setCity("Wrocław");
-		address2.setState("Dolnyśląsk");
-		address2.setZipCode("53506");
-		
-		user.getListOfAddresses().add(address);
-		user.getListOfAddresses().add(address2);
-		
-		/*user.setHomeAddress(address);
-		
-		address.setStreet("Żytnia");
-		address.setCity("Wrocław");
-		address.setState("Dolnyśląsk");
-		address.setZipCode("53506");*/
-		
+		Vehicle vehicle = new Vehicle();
 		
 		user.setUserName("Marta");
-//		user.setOfficeAddress(address);
-		user.setJoinedDate(new Date());
-		user.setDescription("Decription... anything");
+		vehicle.setVehicleName("Audi");
+		user.setVehicle(vehicle);
 		
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
+		
 		session.save(user);
+		session.save(vehicle);
 		session.getTransaction().commit();
-		session.close();
 		
-		user = null;
-		
-		session = sessionFactory.openSession();
-		//session.beginTransaction();
-		user = (UserDetails) session.get(UserDetails.class, 1);
-		//System.out.println("User Name : " + user.getUserName());
 		session.close();
-		System.out.println("Size address list : " + user.getListOfAddresses().size());
 		sessionFactory.close();
 	}
 }
--- src/main/java/com/topyfi/javabrains/HibernateTutorial/dto/UserDetails.java
+++ src/main/java/com/topyfi/javabrains/HibernateTutorial/dto/UserDetails.java
@@ -1,25 +1,12 @@
 package com.topyfi.javabrains.HibernateTutorial.dto;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.Lob;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.hibernate.annotations.CollectionId;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name = "USER_DETAILS")
@@ -29,42 +16,20 @@ public class UserDetails {
 	@GeneratedValue
 	@Column(name = "USER_ID")
 	private int userId;
+	
 	@Column(name = "USER_NAME")
 	private String userName;
+	
+	@OneToOne
+	@JoinColumn(name="VEHICLE_ID")
+	private Vehicle vehicle;
 
-	@Temporal(TemporalType.DATE)
-	private Date joinedDate;
-
-	@ElementCollection(fetch=FetchType.EAGER)
-	@JoinTable(name="USER_ADDRESS",
-				joinColumns = @JoinColumn(name="USER_ID"))
-	private Collection<Address> listOfAddresses = new ArrayList<Address>();
-
-	public Collection<Address> getListOfAddresses() {
-		return listOfAddresses;
+	public Vehicle getVehicle() {
+		return vehicle;
 	}
 
-	public void setListOfAddresses(Collection<Address> listOfAddresses) {
-		this.listOfAddresses = listOfAddresses;
-	}
-
-	@Lob
-	private String description;
-
-	public Date getJoinedDate() {
-		return joinedDate;
-	}
-
-	public void setJoinedDate(Date joinedDate) {
-		this.joinedDate = joinedDate;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
 	}
 
 	public int getUserId() {
