51402e5bbc3b761920d7086774c39f61f2e48d14
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-16 15:32:23 +0200

    after first hibernate inheritance

--- src/main/java/com/topyfi/javabrains/HibernateTutorial/Test.java
+++ src/main/java/com/topyfi/javabrains/HibernateTutorial/Test.java
@@ -4,29 +4,41 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.topyfi.javabrains.HibernateTutorial.dto.FourWheeler;
+import com.topyfi.javabrains.HibernateTutorial.dto.TwoWheeler;
 import com.topyfi.javabrains.HibernateTutorial.dto.UserDetails;
 import com.topyfi.javabrains.HibernateTutorial.dto.Vehicle;
 
 public class Test {
 
 	public static void main(String[] args) {
-		UserDetails user = new UserDetails();
+		//UserDetails user = new UserDetails();
 		Vehicle vehicle = new Vehicle();
-		Vehicle vehicle2 = new Vehicle();
-
-		user.setUserName("Marta");
 		vehicle.setVehicleName("Audi");
-		vehicle2.setVehicleName("BMW");
+		
+		TwoWheeler bike = new TwoWheeler();
+		bike.setVehicleName("Kawasaki");
+		bike.setSteeringHandle("NotWheel");
+		
+		FourWheeler car = new FourWheeler();
+		car.setVehicleName("Mercedes");
+		car.setSteeringWheel("Wheel");
 
-		user.getVehicle().add(vehicle);
-		user.getVehicle().add(vehicle2);
+		//user.setUserName("Marta");
+		//vehicle2.setVehicleName("BMW");
+
+		//user.getVehicle().add(vehicle);
+		//user.getVehicle().add(vehicle2);
 
 		SessionFactory sessionFactory = new Configuration().configure()
 				.buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 
-		session.persist(user);
+		//session.persist(user);
+		session.save(vehicle);
+		session.save(bike);
+		session.save(car);
 		session.getTransaction().commit();
 
 		session.close();
---
+++ src/main/java/com/topyfi/javabrains/HibernateTutorial/dto/TwoWheeler.java
@@ -0,0 +1,17 @@
+package com.topyfi.javabrains.HibernateTutorial.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TwoWheeler extends Vehicle {
+	private String SteeringHandle;
+
+	public String getSteeringHandle() {
+		return SteeringHandle;
+	}
+
+	public void setSteeringHandle(String steeringHandle) {
+		SteeringHandle = steeringHandle;
+	}
+	
+}
