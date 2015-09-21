3bd2b47cbdb96f1935f8cc760417fa6076934bd9
Tomasz Bartnik <tomasz.bartnik93@gmail.com>
2015-09-21 11:56:19 +0200

    checked how 1st level cache works

--- src/main/java/com/topyfi/javabrains/HibernateTutorial/Test.java
+++ src/main/java/com/topyfi/javabrains/HibernateTutorial/Test.java
@@ -9,19 +9,26 @@ import com.topyfi.javabrains.HibernateTutorial.dto.UserDetails;
 public class Test {
 
 	public static void main(String[] args) {
-		UserDetails user = new UserDetails();
-
-		user.setUserName("Marta");
+		
 
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		
-//		session.save(user);
+		UserDetails user = (UserDetails) session.get(UserDetails.class, 1);
+		
 		session.getTransaction().commit();
 		session.close();
 		
-		session.close();
+		Session session2 = sessionFactory.openSession();
+		session2.beginTransaction();
+		
+		UserDetails user2 = (UserDetails) session2.get(UserDetails.class, 1);
+		
+		session2.getTransaction().commit();
+		session2.close();
+		
 		sessionFactory.close();
 	}
 }
